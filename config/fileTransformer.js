f6d87c9723798d859b665d1c56d44e3f8a38632d
James <jimsheen@hotmail.com>
2023-07-16 11:35:53 +0100

    feat: remove unused

--- config/fileTransformer.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-// source: https://jestjs.io/docs/code-transformation#examples
-const path = require('path')
-
-module.exports = {
-  process(src, filename, config, options) {
-    return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';'
-  },
-}
